100ea4674902eb7b963857655b5cd0341d2445d4
Adithya V <adi_101@live.com>
2017-01-07 16:22:09 -0500

    minor changes

--- main/src/App.js
+++ main/src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          Are we ready to win??
         </p>
       </div>
     );
