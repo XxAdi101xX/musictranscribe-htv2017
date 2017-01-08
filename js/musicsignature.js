cdfcda2e86c04711e8a76fcacfb2ed5463d0b1cc
gcttong <gcttong@gmail.com>
2017-01-08 07:50:08 -0500

    added music signature js

---
+++ js/musicsignature.js
@@ -0,0 +1,19 @@
+VF = Vex.Flow;
+
+// Create an SVG renderer and attach it to the Div element named "boo".
+var divv = document.getElementById("boo");
+var renderer = new VF.Renderer(divv, VF.Renderer.Backends.SVG);
+
+// Configure the rendering context.
+renderer.resize(500, 500);
+var context = renderer.getContext();
+context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
+
+// Create a stave of width 400 at position 10, 40 on the canvas.
+var stave = new VF.Stave(10, 40, 400);
+
+// Add a clef and time signature.
+stave.addClef("treble").addTimeSignature("4/4");
+
+// Connect it to the rendering context and draw!
+stave.setContext(context).draw();
